419b74f5eaf6b3d95d39a21a7ed56380c6c5dd55
garryk3 <egoruum@gmail.com>
2019-11-12 17:04:06 +0300

    --

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,23 +1,7 @@
 import { h, Component } from 'preact'
 
-import Loader from './Loader';
-
-import Transport from '../transport';
-
-import { formatNumber } from './Helpers';
-
 class App extends Component {
-    constructor () {
-        super();
-
-        this.transport = new Transport('API_URL');
-    }
-
-    set (key, value) {
-        this[key] = value;
-    }
-
-    render (props, {}) {
+    render (props) {
         return (
             <div class='container' id='container'>
                 BOILERPLATE
